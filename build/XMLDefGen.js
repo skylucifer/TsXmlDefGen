61abe9166acea8486a2a3cc3e16096baec0b0ceb
tarling <james.tarling@gmail.com>
2014-09-03 20:50:02 +0100

    Fixes

--- build/XMLDefGen.js
+++ build/XMLDefGen.js
@@ -4,10 +4,55 @@ var XMLDefGenerator = (function () {
         var data = x2js.xml_str2json(xml, true);
         console.log(data);
 
-        this.analyseChild(data, "Object");
+        this.analyseChild(data, XMLDefGenerator.ROOT_NAME);
 
         console.log(this.lookup);
+
+        this.writeInterfaces();
     }
+    XMLDefGenerator.prototype.indent = function (s) {
+        return "\t" + s;
+    };
+
+    XMLDefGenerator.prototype.makeInterfaceName = function (s) {
+        return "I" + s + "El";
+    };
+
+    XMLDefGenerator.prototype.writeInterfaces = function () {
+        var _this = this;
+        var output = [];
+        for (name in this.lookup) {
+            if (name == XMLDefGenerator.ROOT_NAME)
+                continue;
+
+            var el = this.lookup[name];
+
+            output.push("interface " + this.makeInterfaceName(name));
+            output.push("{");
+
+            var prop = name;
+            if (el.isArray)
+                prop += XMLDefGenerator.ARRAY_SUFFIX;
+
+            el.attributes.forEach(function (item, index, array) {
+                output.push(_this.indent(item + ":string;"));
+            });
+
+            el.children.forEach(function (item, index, array) {
+                var child = _this.lookup[item];
+                var childName = item;
+                var childValue = _this.makeInterfaceName(item);
+                if (child.isArray) {
+                    childName += XMLDefGenerator.ARRAY_SUFFIX;
+                    childValue += "[]";
+                }
+                output.push(_this.indent(childName + ":" + childValue + ";"));
+            });
+            output.push("}");
+        }
+        console.log(output.join("\n"));
+    };
+
     XMLDefGenerator.prototype.analyseChild = function (data, name) {
         var el = this.lookup[name];
 
@@ -22,12 +67,16 @@ var XMLDefGenerator = (function () {
         }
 
         for (var prop in data) {
-            if (prop.replace(/[0-9]/g, "") == "" || prop == "__cnt") {
+            var value = data[prop];
+
+            if (prop == "0") {
+                this.analyseChild(value, name);
+            } else if (prop != "0" && prop.replace(/[0-9]/g, "") == "" || prop == XMLDefGenerator.COUNT_PROP) {
                 continue;
-            } else if (prop.charAt(0) == "_") {
+            } else if (prop.charAt(0) == "_" || typeof value == "string" || (XMLDefGenerator.COUNT_PROP in value && value[XMLDefGenerator.COUNT_PROP] == 0)) {
                 el.attributes.push(prop);
             } else if (prop.indexOf(XMLDefGenerator.ARRAY_SUFFIX) == -1) {
-                this.analyseChild(data[prop], prop);
+                this.analyseChild(value, prop);
                 el.children.push(prop);
 
                 var arrayData = data[prop + XMLDefGenerator.ARRAY_SUFFIX];
@@ -39,6 +88,8 @@ var XMLDefGenerator = (function () {
         return el;
     };
     XMLDefGenerator.ARRAY_SUFFIX = "_asArray";
+    XMLDefGenerator.COUNT_PROP = "__cnt";
+    XMLDefGenerator.ROOT_NAME = "Object";
     return XMLDefGenerator;
 })();
 
--- src/XMLDefGenerator.ts
+++ src/XMLDefGenerator.ts
@@ -5,15 +5,64 @@ class XMLDefGenerator {
     private lookup:{[name:string]:IElement} = {};
     
     private static ARRAY_SUFFIX:string = "_asArray";
+    private static COUNT_PROP:string = "__cnt";
+    private static ROOT_NAME:string = "Object";
     
     constructor(xml:string) {
         
         var data:any = x2js.xml_str2json(xml, true);
         console.log(data);
         
-        this.analyseChild(data, "Object");
+        this.analyseChild(data, XMLDefGenerator.ROOT_NAME);
         
         console.log(this.lookup);
+        
+        this.writeInterfaces();
+    }
+    
+    private indent(s:string):string {
+        return "\t" + s;
+    }
+    
+    private makeInterfaceName(s:string):string {
+        return "I" + s + "El";
+    }
+    
+    private writeInterfaces():void {
+        
+        var output:string[] = [];
+        for (name in this.lookup)
+        {
+            if (name == XMLDefGenerator.ROOT_NAME) continue;
+            
+            var el:IElement = this.lookup[name];
+            
+            output.push("interface " + this.makeInterfaceName(name));
+            output.push("{");
+            
+            var prop:string = name;
+            if (el.isArray) prop += XMLDefGenerator.ARRAY_SUFFIX;
+            
+            el.attributes.forEach((item, index, array)=>{
+                output.push(this.indent(item + ":string;"));
+            });
+            
+            el.children.forEach((item, index, array)=>{
+                
+                var child:IElement = this.lookup[item];
+                var childName:string = item;
+                var childValue:string = this.makeInterfaceName(item);
+                if (child.isArray)
+                {
+                    childName += XMLDefGenerator.ARRAY_SUFFIX;
+                    childValue += "[]";
+                }
+                output.push(this.indent(childName + ":" + childValue + ";"));
+            });
+            output.push("}");
+        }
+        console.log(output.join("\n"));
+        
     }
     
     private analyseChild(data:any, name:string):IElement {
@@ -33,18 +82,27 @@ class XMLDefGenerator {
             el.isArray = true;    
         }*/
         
+            
+        
         for (var prop in data)
         {
-            if (prop.replace(/[0-9]/g, "") == "" || prop == "__cnt")
+            
+            var value:any = data[prop];
+            
+            if (prop == "0")
+            {
+                this.analyseChild(value, name);
+            } else if (prop != "0" && prop.replace(/[0-9]/g, "") == "" || prop == XMLDefGenerator.COUNT_PROP)
             {
                 continue;
-            } else if (prop.charAt(0) == "_")
+            } else if (prop.charAt(0) == "_" || typeof value == "string" || (XMLDefGenerator.COUNT_PROP in value && value[XMLDefGenerator.COUNT_PROP] == 0))
             {
                 el.attributes.push(prop);    
             } else if (prop.indexOf(XMLDefGenerator.ARRAY_SUFFIX)==-1){
-                this.analyseChild(data[prop], prop)
-                el.children.push(prop);
                 
+                this.analyseChild(value, prop)
+                el.children.push(prop);
+
                 var arrayData:any[] = data[prop + XMLDefGenerator.ARRAY_SUFFIX];
                 if (arrayData && arrayData.length > 1)
                 {
